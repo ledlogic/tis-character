db9c2a3ce36eaff4e16f59137d6acde186c27604
Jeff Conrad <ledlogic@gmail.com>
2020-12-26 13:24:07 -0600

    Add stat mods

--- js/tis-stats.js
+++ js/tis-stats.js
@@ -2,23 +2,31 @@
 
 tis.stats = {
 	levels: [20, 12, 10, 8, 6, 4],
+	mods: [0, 0, 0, 0, 0, 0],
 	map: {},
 	init: function() {
 		tis.log("tis.stats.init");
 	},
-	set: function(arr) {
-		tis.log(["arr", arr]);
-		var stats = arr.split(" ");
+	set: function(st, m) {
+		tis.log(["st", st]);
+		tis.log(["m", m]);
 		
-		for (var i=0; i<stats.length; i++) {
-			var name = stats[i].toLowerCase();
-			var die = tis.stats.levels[i];
-			var stat = {
-				name: name,
-				die: die
-			}
-			tis.stats.map[name] = stat;
-			$("#tis_" + name).val(die);
-		}		
+		if (st) {
+			var stats = st.split(" ");
+			for (var i=0; i<stats.length; i++) {
+				var name = stats[i].toLowerCase();
+				var die = tis.stats.levels[i];
+				var mod = tis.stats.mods[i];
+				var stat = {
+					name: name,
+					die: die,
+					mod: mod
+				}
+				tis.stats.map[name] = stat;
+				$("#tis_" + name).val(die);
+				var modStr = (mod > -1 ? "+" : "") + mod;
+				$("#tis_" + name + "_mod").val(modStr);
+			}		
+		}
 	}
 };
