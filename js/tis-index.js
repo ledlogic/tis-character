1872075537f55a34838a03f04dfd8fdf9c4af708
Jeff Conrad <ledlogic@gmail.com>
2021-01-02 10:58:30 -0600

    Added datatables for sorting

--- js/tis-index.js
+++ js/tis-index.js
@@ -1,62 +1,80 @@
 tis.init = function() {
-	$(".tis-saved-characters").html("<p>No saved characters</p>");
-	
-	tis.characters = tis.localstorage.getCharacters();
-	tis.log(["tis.characters", tis.characters]);
+	tis.index.init();
+};
 
-	var html = [];
-	if (tis.characters.length) {
-		html.push("<table border=\"1\" width=\"100%\"><theader><tr>"
-			+ "<th>Load</th>"
-			+ "<th>Delete</th>"
-		 	+ "<th>Character Name</th>"
-		 	+ "<th>Species</th>"
-		 	+ "<th>Trope</th>"
-		 	+ "<th>Date Saved</th>"
-		 	+ "</tr></theader><tbody>"
-		);
-	
-		_.each(tis.characters, function(timekey) {
-			tis.log(["timekey", timekey]);
-			var characterItem = localStorage.getItem(timekey);
-			if (characterItem) {
-				var character = JSON.parse(characterItem);
-				tis.log(["character", character]);
-			
-				html.push("<tr>"
-				 	+ "<td align=\"center\">"
-				 	+ "<img class=\"tis-index-icon tis-load\" data-timekey=\"" + timekey + "\" src=\"img/bootstrap/person.svg\" alt=\"Load\" title=\"Load\" />"
-				 	+ "</td>"
-				 	+ "<td align=\"center\">"
-				 	+ "<img class=\"tis-index-icon tis-delete\" data-timekey=\"" + timekey + "\" src=\"img/bootstrap/person-dash.svg\" alt=\"Delete\" title=\"Delete\" />"
-				 	+ "</td>"
-				 	+ "<td>" + character.name + "</td>"
-				 	+ "<td>" + character.species + "</td>"
-				 	+ "<td>" + character.trope + "</td>"
-				 	+ "<td>" + tis.localstorage.timekeyToDate(timekey) + "</td>"
-				);
-			} else {
-				tis.log("Could not find item, timekey[" + timekey + "]");
-			}
-		});
-	
-		if (tis.characters.length) {
-			html.push("</tbody></table>");
-		}	
-		$(".tis-saved-characters").html(html.join("\n"));
+tis.index = {
+	init: function() {
+		$(".tis-saved-characters").html("<p>No saved characters</p>");
 		
-		$(".tis-load").on("click", function() {
-			var timekey = $(this).data("timekey");
-			window.location = "tis-load.html?timekey=" + encodeURIComponent(timekey);
-		});
+		tis.characters = tis.localstorage.getCharacters();
+		tis.log(["tis.characters", tis.characters]);
+	
+		var html = [];
+		if (tis.characters.length) {
+			html.push("<table border=\"1\" width=\"100%\"><thead><tr>"
+			 	+ "<th>Character Name</th>"
+			 	+ "<th>Species</th>"
+			 	+ "<th>Trope</th>"
+			 	+ "<th>Date Saved</th>"
+				+ "<th data-orderable=\"false\" >Load</th>"
+				+ "<th data-orderable=\"false\" >Delete</th>"
+			 	+ "</tr></thead><tbody>"
+			);
+		
+			_.each(tis.characters, function(timekey) {
+				tis.log(["timekey", timekey]);
+				var characterItem = localStorage.getItem(timekey);
+				if (characterItem) {
+					var character = JSON.parse(characterItem);
+					tis.log(["character", character]);
 
-		$(".tis-delete").on("click", function() {
-			var timekey = $(this).data("timekey");
-			var character = tis.localstorage.deleteCharacter(timekey);
+					var date = tis.localstorage.timekeyToDate(timekey);
+					var dateStr = date.toLocaleString();
+				
+					html.push("<tr>"
+					 	+ "<td>" + character.name + "</td>"
+					 	+ "<td>" + character.species + "</td>"
+					 	+ "<td>" + character.trope + "</td>"
+					 	+ "<td>" + dateStr + "</td>"
+					 	+ "<td align=\"center\">"
+					 	+ "<img class=\"tis-index-icon tis-load\" data-sort=\"" + timekey + "\" data-timekey=\"" + timekey + "\" src=\"img/bootstrap/person.svg\" alt=\"Load\" title=\"Load\" />"
+					 	+ "</td>"
+					 	+ "<td align=\"center\">"
+					 	+ "<img class=\"tis-index-icon tis-delete\" data-sort=\"" + timekey + "\" data-timekey=\"" + timekey + "\" src=\"img/bootstrap/person-dash.svg\" alt=\"Delete\" title=\"Delete\" />"
+					 	+ "</td>"
+					 	+ "</tr>"
+					);
+				} else {
+					tis.log("Could not find item, timekey[" + timekey + "]");
+				}
+			});
+		
+			if (tis.characters.length) {
+				html.push("</tbody></table>");
+			}	
+			var h = html.join("\n");
+			console.log(h);
+			$(".tis-saved-characters").html(h);
+			var options = {
+				//info: false,
+			    //ordering: false,
+				paging: false
+			};
+			$('.tis-saved-characters table').DataTable(options);
 			
-			alert("Deleted " + character.name);
-			
-			tis.init();
-		});
+			$(".tis-load").on("click", function() {
+				var timekey = $(this).data("timekey");
+				window.location = "tis-load.html?timekey=" + encodeURIComponent(timekey);
+			});
+	
+			$(".tis-delete").on("click", function() {
+				var timekey = $(this).data("timekey");
+				var character = tis.localstorage.deleteCharacter(timekey);
+				
+				alert("Deleted " + character.name);
+				
+				tis.init();
+			});
+		};
 	}
 };
